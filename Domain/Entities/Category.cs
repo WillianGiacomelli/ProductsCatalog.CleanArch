264ef6b01cd1ed62ce37017111fc2cc152931256
Willian Giacomelli <willian.giacomelli@iatec.com>
2024-05-23 16:05:51 -0300

    feat: initial commit from project domain

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public sealed class Category
+{
+    public int CategoryId { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public ICollection<Product> Products { get; private set; }
+
+    public Category(string name, string description)
+    {
+        ValidateCategoryDomain(name);
+        Description = description;
+    }
+
+    public Category(int id, string name, string description)
+    {
+        CategoryId = id;
+        Name = name;
+        Description = description;
+    }
+
+    private void ValidateCategoryDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name -  Name is required");
+
+        DomainExceptionValidation.When(name.Length <= 3, "Invalid name -  Name needs to be greater than 4 characters");
+        Name = name;
+    }
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public sealed class Product
+{
+    public int ProductId { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public decimal Price { get; private set; }
+    public int Stock { get; private set; }
+    public string Image { get; private set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
