c91d8f4526e969ba87105f142558cf71b0185776
Willian Giacomelli <willian.giacomelli@iatec.com>
2024-06-10 13:47:09 -0300

    feat: add product service

---
+++ Application/Services/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Interfaces;
+
+namespace Application.Services;
+
+public class ProductService : IProductService
+{
+    private IProductRepository _repository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public Task Add(ProductDTO productDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductDTO> GetProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductDTO> GetProductCategory(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ProductDTO>> GetProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Remove(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(ProductDTO productDTO)
+    {
+        throw new NotImplementedException();
+    }
+}
